bd76b129b7f5554961fefc3b311a2ad01011ee6f
satoshinakamototoken <83735709+satoshinakamototoken@users.noreply.github.com>
2021-07-02 22:47:47 -0300

    Update Context.sol

--- Context.sol
+++ Context.sol
@@ -1,15 +1,13 @@
-// SPDX-License-Identifier: Unlicensed
+// SPDX-License-Identifier: MIT
 
-
-pragma solidity ^0.6.12;
+pragma solidity ^0.8.4;
 
 abstract contract Context {
-    function _msgSender() internal view virtual returns (address payable) {
+    function _msgSender() internal view virtual returns (address) {
         return msg.sender;
     }
-
-    function _msgData() internal view virtual returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this;
         return msg.data;
     }
 }
